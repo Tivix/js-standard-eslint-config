abb523e6980ed387e0e2401875df77ae3b472646
Krzysztof Przybylski <krzysztof.przybylski@tivix.com>
2020-01-07 13:03:29 +0100

    Add typescript rules

--- rules/typescript.js
+++ rules/typescript.js
@@ -1,48 +1,54 @@
-const { join } = require('path');
-
 module.exports = () => {
-  const tsconfigPath = './tsconfig.json';
+  const tsconfigPath = "./tsconfig.json";
 
   return {
-    files: ['*.ts', '*.tsx'],
+    files: ["*.ts", "*.tsx"],
     extends: [
-      'plugin:import/typescript',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      'prettier/@typescript-eslint',
+      "plugin:import/typescript",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "prettier/@typescript-eslint"
     ],
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
     parserOptions: {
-      project: tsconfigPath,
+      project: tsconfigPath
     },
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 0,
-      '@typescript-eslint/no-explicit-any': 2,
-      'import/no-default-export': 2,
-      'import/prefer-default-export': 0,
-      'react/jsx-filename-extension': 0,
-      'react/prop-types': 0,
-      'react/jsx-props-no-spreading': 0,
+      "@typescript-eslint/explicit-function-return-type": [
+        "warn",
+        {
+          allowTypedFunctionExpressions: true,
+          allowExpressions: true,
+          allowHigherOrderFunctions: true
+        }
+      ],
+      "@typescript-eslint/prefer-optional-chain": "warn",
+      "@typescript-eslint/no-explicit-any": 2,
+      "import/no-default-export": 2,
+      "import/prefer-default-export": 0,
+      "react/jsx-filename-extension": 0,
+      "react/prop-types": 0,
+      "react/jsx-props-no-spreading": 0
     },
     settings: {
-      'import/parsers': {
-        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"]
       },
-      'import/resolver': {
+      "import/resolver": {
         node: {},
         ts: {
-          directory: tsconfigPath,
-        },
-      },
+          directory: tsconfigPath
+        }
+      }
     },
     overrides: [
       {
-        files: ['*.stories.*'],
+        files: ["*.stories.*"],
         rules: {
-          'import/no-default-export': 0,
-        },
-      },
-    ],
+          "import/no-default-export": 0
+        }
+      }
+    ]
   };
 };
