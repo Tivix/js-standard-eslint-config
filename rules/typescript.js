0155c6860aec8d906278195b36074be8c9eed737
Krzysztof Przybylski <krzysztof.przybylski@tivix.com>
2021-08-05 15:29:11 +0200

    fix: remove prettier from TS config

--- rules/typescript.js
+++ rules/typescript.js
@@ -8,7 +8,6 @@ module.exports = () => {
       "plugin:@typescript-eslint/eslint-recommended",
       "plugin:@typescript-eslint/recommended",
       "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      "prettier/@typescript-eslint"
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
